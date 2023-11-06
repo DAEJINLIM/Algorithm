5735a89e4cf4c0a794b2d7bcb41aaaf63425a383
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2023-11-06 21:33:12 +0900

    [level unrated] Title: flag에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.swift
+++ 프로그래머스/unrated/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ a:Int, _ b:Int, _ flag:Bool) -> Int {
-    var result = Int()
-    if flag {
-        result = a + b
-    } else {
-        result = a - b
-    }
-    return result
+    return flag ? a + b : a - b
 }
