38a26b1e29ce7984e73c2d4b6ab2ff798781014e
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2023-11-16 21:23:55 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> Int {
-    var a = [0,1,2,3,4,5,6,7,8,9]
-    for i in numbers {
-        a = a.filter { $0 != i }
-    }
-    return a.reduce(0, +)
+    return 45 - numbers.reduce(0, +)
 }
