775aa65d1e8a6f87793d8f3b0da5bc7ed5cf13ae
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2023-11-17 23:21:26 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
@@ -2,16 +2,14 @@ import Foundation
 
 func solution(_ n:Int) -> Int {
     var a = n
-    var s = Int()
     var k = [Int]()
     while a >= 1 {
         k.append(a % 3)    
         a = a / 3
     }
-    print(k)
     for (i, n) in k.enumerated() {
         let k = k.count - i - 1
-        s += n * Int(pow(3.0, Double(k)))
+        a += n * Int(pow(3.0, Double(k)))
     }
-    return s
+    return a
 }
