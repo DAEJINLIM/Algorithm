c91e319502004d9dad4164249aa8d66de6945422
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2024-05-08 23:09:13 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 12 ms, Memory: 79504 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.swift
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.swift
@@ -1,13 +1,9 @@
 import Foundation
-var strings = [String]()
+
 while true {
     let input = readLine() ?? ""
     if input.isEmpty {
         break
     }
-    strings.append(input)
-}
-
-for i in strings {
-    print(i)
+    print(input)
 }
