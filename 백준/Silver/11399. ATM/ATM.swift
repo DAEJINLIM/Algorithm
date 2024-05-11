4ead979218d995d72f9b6abcf928ab4980385549
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2024-05-11 14:37:59 +0900

    [Silver IV] Title: ATM, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.swift
+++ 백준/Silver/11399. ATM/ATM.swift
@@ -2,14 +2,14 @@ import Foundation
 
 let input = Int(readLine()!)!
 
-var people = readLine()!.split(separator: " ").map { Int($0) }
-people.sort(by: {$0! < $1!})
+var people = readLine()!.split(separator: " ").map { Int($0)! }
+people.sort(by: {$0 < $1})
 
 var result = 0
 
 for i in 0..<input {
     for k in 0...i {
-        result += people[k]!
+        result += people[k]
     }
 }
 
