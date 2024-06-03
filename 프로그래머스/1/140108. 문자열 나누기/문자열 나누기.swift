c23474e18910d3b85e7a66b98aeb9e0e54a46bc8
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2024-06-03 20:48:01 +0900

    [level 1] Title: 문자열 나누기, Time: 2.44 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.swift
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.swift
@@ -1,43 +1,23 @@
 import Foundation
 
 func solution(_ s:String) -> Int {
-    var first = String(s.first!)
-    var result = [String]()
-    var arr = s.map { String($0) }
+    var result = 0
+    var first: Character?
+    var count = 0
     
-    var same = 0
-    var different = 0
-    var word = ""
-    
-    while !arr.isEmpty {
-        for (index, i) in arr.enumerated() {
-            if first == i {
-                same += 1
-                word += i
-            } else {
-                different += 1
-                word += i
-            }
-            
-            arr.removeFirst()
-            
-            if same == different {
-                result.append(word)
-                if arr.isEmpty {
-                    break
-                }
-                first = arr.first!
-                (same, different) = (0, 0)
-                word = ""
-                
-                break
-            }
-            
-            if arr.isEmpty {
-                result.append(word)
-            }
+    for i in s {
+        if first == nil {
+            first = i
+            result += 1
+        }
+        
+        count += first == i ? 1 : -1
+        
+        if count == 0 {
+            first = nil
         }
     }
     
-    return result.count
+    
+    return result
 }
