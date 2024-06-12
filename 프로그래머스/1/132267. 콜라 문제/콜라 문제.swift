925683200b332e1ba6f663195ab968c950c2686d
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2024-06-12 22:26:43 +0900

    [level 1] Title: 콜라 문제, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.swift
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.swift
@@ -1,14 +1,5 @@
 import Foundation
 
 func solution(_ a:Int, _ b:Int, _ n:Int) -> Int {
-    var EmptyCoke = n
-    var count = 0
-    while true {
-        count += (EmptyCoke / a) * b
-        EmptyCoke = EmptyCoke % a == 0 ? (EmptyCoke / a) * b : (EmptyCoke / a) * b + (EmptyCoke % a)
-        if EmptyCoke < a {
-            break
-        }
-    }
-    return count
+    return (n > b ? n - b : 0) / (a - b) * b
 }
