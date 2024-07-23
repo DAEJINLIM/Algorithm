6106c5e004a4ac0fff13a3409988e87f31a56762
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2024-07-24 05:20:32 +0900

    [level 2] Title: 프로세스, Time: 3.55 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+func solution(_ priorities:[Int], _ location:Int) -> Int {
+    var queue = priorities
+    var loca = location
+    var count = 0
+    
+    while !queue.isEmpty {
+        let max = queue.max() ?? 0
+        let pop = queue.removeFirst()
+        if pop >= max {
+            count += 1
+            if loca == 0 {
+                return count
+            }
+        } else {
+            queue.append(pop)
+        }
+        if !queue.isEmpty {
+            loca -= 1
+        }
+        if loca < 0 {
+            loca = queue.count - 1
+        }
+    }
+    
+    return priorities.count
+}
