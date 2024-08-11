43689474b6dc6f3afa1b44ea8be2756dee3d8de2
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2024-08-11 21:04:54 +0900

    [level 2] Title: 요격 시스템, Time: 2249.38 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.swift
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.swift
@@ -1,17 +1,13 @@
 import Foundation
 
 func solution(_ targets:[[Int]]) -> Int {
-    var count = 1
+    var count = 0
     var m = 0
     
     for target in targets.sorted { $0[1] < $1[1] } {
-        if m == 0 {
+        if m <= target[0] {
             m = target[1]
-        } else {
-            if m <= target[0] {
-                m = target[1]
-                count += 1
-            }
+            count += 1
         }
     }
     
