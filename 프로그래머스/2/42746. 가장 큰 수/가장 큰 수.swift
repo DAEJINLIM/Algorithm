7ed45b661ba49115fbc681090c619f08278bc556
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2024-08-31 22:46:21 +0900

    [level 2] Title: 가장 큰 수, Time: 559.09 ms, Memory: 21 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
@@ -2,19 +2,10 @@ import Foundation
 
 func solution(_ numbers:[Int]) -> String {
     if numbers.reduce(0, +) == 0 { return "0" }
-    var dic = [Int:[Int]]()
     var result = ""
     
-    for i in numbers {
-        dic[Int(String(String(i).first!))!, default: [Int]()].append(i)
-    }
-    
-    for i in dic.sorted { $0.key > $1.key } {
-        let value = i.value.sorted { String($0)+String($1) > String($1)+String($0) }
-        
-        for j in value {
-            result.append(String(j))
-        }
+    for i in numbers.sorted { String($0)+String($1) > String($1)+String($0) } {
+            result.append(String(i))
     }
     return result
 }
