df4bc5e71fe545572ba5db00f88ab12e25dec14d
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2025-02-08 03:26:23 +0900

    [level 2] Title: 큰 수 만들기, Time: 219.68 ms, Memory: 20 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+
+func solution(_ number:String, _ k:Int) -> String {
+    var stack: [Character] = []
+    var removeCount = k
+    
+    for digit in number {
+        while removeCount > 0, !stack.isEmpty, stack.last! < digit {
+            stack.removeLast()
+            removeCount -= 1
+        }
+        stack.append(digit)
+    }
+    return String(stack.prefix(number.count - k))
+}
