2141b06622b6b559070e5f02860ae36c366367d3
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2025-02-08 04:26:52 +0900

    [level 2] Title: 시소 짝꿍, Time: 31.84 ms, Memory: 21.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+func solution(_ weights:[Int]) -> Int64 {
+    var result: Int64 = 0
+    var countMap = [Double: Int]()
+    let ratios = [2.0/3.0, 2.0/4.0, 3.0/4.0]
+    
+    for i in weights {
+        countMap[Double(i), default: 0] += 1
+    }
+    
+    for (weight, count) in countMap.sorted(by: { $0.key > $1.key }) {
+        if count > 1 {
+            result += Int64(count * (count - 1) / 2)
+        }
+        
+        for ratio in ratios {
+            let target = Double(weight) * ratio
+            if let targetCount = countMap[target] {
+                result += Int64(count * targetCount)
+            }
+        }
+    }
+    
+    return result
+}
