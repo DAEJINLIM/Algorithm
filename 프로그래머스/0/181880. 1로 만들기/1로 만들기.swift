0b86a3a72a91cb3806cfa10bd4b302c1da0750fa
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2025-10-15 21:32:23 +0900

    [level 0] Title: 1로 만들기, Time: 0.04 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ num_list:[Int]) -> Int {
+    var result = 0
+    for i in num_list {
+        var num = i
+        while num != 1 {
+            num /= 2
+            result += 1
+        }
+    }
+    return result
+}
