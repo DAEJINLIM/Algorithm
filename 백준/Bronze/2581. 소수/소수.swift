355ee3ec8c1899f5b1cdf27fb4a1f0ef53ecf44e
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-03-24 01:17:06 +0900

    [Bronze II] Title: 소수, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.swift
+++ 백준/Bronze/2581. 소수/소수.swift
@@ -2,30 +2,32 @@ import Foundation
 
 let m = Int(readLine()!)!
 let k = Int(readLine()!)!
-var sum = 0
-var minimum = 10000
 
-func solution(_ n: Int) -> Bool {
-    if n < 2 { return false }
-    if n == 2 { return true }
-    if n % 2 == 0 { return false }
-    
-    let limit = Int(Double(n).squareRoot())
-    
-    for i in stride(from: 3, through: limit, by: 2) {
-        if n % i == 0 {
-            return false
+var isPrime = Array(repeating: true, count: k + 1)
+var result: [Int] = []
+
+if k >= 0 { isPrime[0] = false }
+if k >= 1 { isPrime[1] = false }
+
+if k > 2 {
+    for i in 2...Int(Double(k).squareRoot()) {
+        if isPrime[i] {
+            for j in stride(from: i * i, through: k, by: i) {
+                isPrime[j] = false
+            }
         }
     }
-    
-    return true
 }
 
 for i in m...k {
-    if solution(i) {
-        sum += i
-        minimum = min(minimum, i)
+    if isPrime[i] {
+        result.append(i)
     }
 }
 
-print(sum == 0 ? -1 : "\(sum)\n\(minimum)")
+if result.isEmpty {
+    print(-1)
+} else {
+    print(result.reduce(0, +))
+    print(result.min()!)
+}
