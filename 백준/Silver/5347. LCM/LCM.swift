10d68026a6db4eafc7b9a4ff466ba15a72c75e8f
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-03-27 22:29:42 +0900

    [Silver V] Title: LCM, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+for _ in 0..<n {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    let (a,b) = (input[0], input[1])
+    
+    print(a/gcd(a,b)*b)
+}
+
+func gcd(_ a: Int, _ b: Int) -> Int {
+    if b == 0 {
+        return a
+    } else {
+        return gcd(b, a % b)
+    }
+}
+
+
+
