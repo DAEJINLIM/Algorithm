be938576e4cc7ae3a1a4e95d592695e692ebf0b8
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-03-30 22:24:08 +0900

    [Silver V] Title: 거스름돈, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+let input = Int(readLine()!)!
+var n = input
+var count = 0
+
+count += n / 5
+n = n % 5
+
+while true {
+    if n % 2 == 0 {
+        count += n / 2
+        print(count)
+        break
+    } else {
+        n += 5
+        count -= 1
+        if count < 0 {
+            print(-1)
+            break
+        }
+    }
+}
