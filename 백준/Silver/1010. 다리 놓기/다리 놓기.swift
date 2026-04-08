44849fe332e58c654e02049a57dd49720bd63453
DAEJINLIM <115560272+DAEJINLIM@users.noreply.github.com>
2026-04-08 23:48:15 +0900

    [Silver V] Title: 다리 놓기, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.swift
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.swift
@@ -1,27 +1,17 @@
-import Foundation
 
-let input = Int(readLine()!)!
+let n = Int(readLine()!)!
 
-for _ in 0..<input {
-    let input = readLine()!.split(separator: " ")
-    let N = Int(input[0])!
-    let M = Int(input[1])!
+for _ in 0..<n {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    let a = input[0]
+    let b = input[1]
     
-    if N == M {
-        print("1")
-    } else if N == 1 {
-        print(M)
-    } else {
-        var a = "1"
-        var b = "1"
-        
-        for i in M-N+1...M {
-            a = NSDecimalNumber(string: String(a)).multiplying(by: NSDecimalNumber(string: String(i))).stringValue
-        }
-        for i in 1...N {
-            b = NSDecimalNumber(string: String(b)).multiplying(by: NSDecimalNumber(string: String(i))).stringValue
-        }
-        
-        print(NSDecimalNumber(string: a).dividing(by: NSDecimalNumber(string: b)))
+    var result = 1
+    
+    for i in 0..<a {
+        result *= (b - i)
+        result /= (i + 1)
     }
+    
+    print(result)
 }
